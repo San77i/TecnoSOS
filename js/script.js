2adaca07e03f4f0559996e9ae74b222123765c60
San77i <sanattaque@gmail.com>
2025-06-13 01:18:32 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const calcularCotizacion = async () => {
     };
 
     try {
-        const response = await fetch("https://tecnosos.onrender.com", {
+        const response = await fetch("https://tecnosos.onrender.com/Cotizar", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
